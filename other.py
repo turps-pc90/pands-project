1f12963e537af6557bd8ac92c47ec308582dc71f
pc-hm20 <pcallaghan@homecaremedical.ie>
2023-05-07 20:33:46 +0100

    Added other.py - a script for main menu option 4 which will allow user select some visualisation options from Seaborn instead of Matplotlib

---
+++ other.py
@@ -0,0 +1,8 @@
+# other.py
+# This script will display a selection of Seaborn data visualisation options for user. Based on the Kaggle guide here - https://www.kaggle.com/code/rakesh6184/seaborn-plot-to-visualize-iris-data/notebook
+# 
+# Author: Paul Callaghan
+
+import pandas as pd
+import seaborn as sns
+
